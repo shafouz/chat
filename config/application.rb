6b35d0ce4f4cbaf3c78cbdc79455e5b6956d17e6
shafouz <jlmacedomatos@gmail.com>
2021-07-26 14:38:22 -0400

    Fixed env stuff

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,14 @@ Bundler.require(*Rails.groups)
 
 module Chat
   class Application < Rails::Application
+
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exist?(env_file)
+    end 
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
@@ -34,12 +42,5 @@ module Chat
 
     # Don't generate system test files.
     config.generators.system_tests = nil
-
-    config.before_configuration do
-      env_file = File.join(Rails.root, 'config', 'env.yml')
-      YAML.load(File.open(env_file)).each do |key, value|
-        ENV[key.to_s] = value
-      end if File.exist?(env_file)
-    end 
   end
 end
