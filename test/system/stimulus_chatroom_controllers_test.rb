f53521d2dffefb860d9070db2bb188ab641cfb79
shafouz <jlmacedomatos@gmail.com>
2021-08-12 11:46:02 -0400

    Figuring capybara/system tests out.

--- test/system/stimulus_chatroom_controllers_test.rb
+++ test/system/stimulus_chatroom_controllers_test.rb
@@ -1,10 +1,23 @@
 require "application_system_test_case"
 
 class StimulusChatroomControllersTest < ApplicationSystemTestCase
-  test "visiting the index" do
-    visit stimulus_chatroom_controllers_url
+  def setup
+    User.create(name: 'abc', email: 'abc@abc.com', password: 'secret')
+    Chatroom.create(name: 'chat1')
+  end
 
-    assert_selector "p", class: "notice"
-    puts "********************************************************************************************************************************************************************************************************"
+  test "visiting chatroom 1" do
+    visit sign_in_path
+    fill_in('email', with: 'abc@abc.com')
+    fill_in('password', with: 'secret')
+    click_on('commit')
+    assert true, page.has_content?('Logged in successfully')
+
+    click_on('Chatrooms')
+    click_on('New Chatroom')
+    fill_in('chatroom[name]', with: 'chat1')
+    click_on('commit')
+    assert true, page.has_content?('chat1')
+    assert true, page.has_content?('abc')
   end
 end
